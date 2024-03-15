f089e5ba35ee51abb6e827e47ad377c5e46c8b1a
eseus-coder <arindambose975@gmail.com>
2024-03-15 15:32:14 +0530

    - Binary search practice questions

---
+++ src/searchingAlgos_03/binarySearch_03_02/two_dimensional_array_03_02_13/Fundamental_Unsorted_Search_Linear.java
@@ -0,0 +1,9 @@
+package searchingAlgos_03.binarySearch_03_02.two_dimensional_array_03_02_13;
+
+public class Fundamental_Unsorted_Search_Linear {
+    public static void main(String[] args) {
+        //Refer: array_arrayList_02/two_dimensional_array_02_02/ArrayInput.java
+        //Refer: searchingAlgos_03/linearSearch_03_01/SearchIn2D.java
+        //Refer: searchingAlgos_03/linearSearch_03_01/SearchInRange.java
+    }
+}
